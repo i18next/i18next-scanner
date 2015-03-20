df26684c8390bda9d1ff689b07294ebd72fb9184
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2015-03-20 02:12:39 -0700

    Bug fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 var path = require('path');
 var gulp = require('gulp');
 var del = require('del');
-var browserify = require('browserify');
 var runSequence = require('run-sequence');
 var jshint = require('gulp-jshint');
 var source = require('vinyl-source-stream');
