ac6d61cc189cad53c1a4b6bc5b05374b10ea02be
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2015-03-24 22:50:26 -0700

    Adding support for Grunt

---
+++ tasks/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(grunt) {
+    var i18next = require('..');
+    var vfs = require('vinyl-fs');
+
+    grunt.registerMultiTask('i18next', 'A grunt task for i18next-scanner', function() {
+        var done = this.async();
+        var options = this.options() || {};
+        var targets = (this.file) ? [this.file] : this.files;
+
+        targets.forEach(function(target) {
+            vfs.src(target.files || target.src, {base: target.base || '.'})
+                .pipe(i18next(options, target.customTransform, target.customFlush))
+                .pipe(vfs.dest(target.dest))
+                .on('end', function() {
+                    done();
+                });
+        });
+    });
+};
