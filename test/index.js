dee89ed9a1750fbcf84c079b337916716ec481f6
Cheton Wu <cheton@gmail.com>
2015-12-09 15:28:59 +0800

    Update test script

--- test/index.js
+++ test/index.js
@@ -1,13 +1,107 @@
 'use strict';
 
+var _ = require('lodash');
+var fs = require('fs');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var path = require('path');
 var test = require('tap').test;
-var scanner = require('../');
+var i18nextScanner = require('../');
+var hash = require('sha1');
+var table = require('text-table');
 
-var toFixturePath = function(fileName) {
-    var args = Array.prototype.slice.call(arguments);
-    return path.resolve.apply(null, [__dirname, 'fixtures'].concat(args));
+var customTransform = function _transform(file, enc, done) {
+    var parser = this.parser;
+    var extname = path.extname(file.path);
+    var content = fs.readFileSync(file.path, enc);
+    var parseResults = [
+        ['Key', 'Value']
+    ];
+
+    gutil.log('parsing ' + JSON.stringify(file.relative) + ':');
+
+    // Using i18next-text
+    (function() {
+        var results = content.match(/i18n\._\(("[^"]*"|'[^']*')\s*[\,\)]/igm) || '';
+        _.each(results, function(result) {
+            var key, value;
+            var r = result.match(/i18n\._\(("[^"]*"|'[^']*')/);
+
+            if (r) {
+                value = _.trim(r[1], '\'"');
+
+                // Replace double backslash with single backslash
+                value = value.replace(/\\\\/g, '\\');
+                value = value.replace(/\\\'/, '\'');
+
+                key = hash(value); // returns a hash value as its default key
+
+                parser.parse(key, value);
+                parseResults.push([key, _.isUndefined(value) ? '' : value ]);
+            }
+        });
+    }());
+
+    // i18n function helper
+    (function() {
+        var results = content.match(/{{i18n\s+("(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*')?([^}]*)}}/gm) || [];
+        _.each(results, function(result) {
+            var key, value;
+            var r = result.match(/{{i18n\s+("(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*')?([^}]*)}}/m) || [];
+
+            if ( ! _.isUndefined(r[1])) {
+                value = _.trim(r[1], '\'"');
+
+                // Replace double backslash with single backslash
+                value = value.replace(/\\\\/g, '\\');
+                value = value.replace(/\\\'/, '\'');
+            }
+
+            var params = parser.parseHashArguments(r[2]);
+            if (_.has(params, 'defaultKey')) {
+                key = params['defaultKey'];
+            }
+
+            if (_.isUndefined(key) && _.isUndefined(value)) {
+                return;
+            }
+
+            if (_.isUndefined(key)) {
+                key = hash(value); // returns a hash value as its default key
+            }
+
+            parser.parse(key, value);
+            parseResults.push([key, _.isUndefined(value) ? '' : value ]);
+        });
+    }());
+
+    // i18n block helper
+    (function() {
+        var results = content.match(/{{#i18n\s*([^}]*)}}((?:(?!{{\/i18n}})(?:.|\n))*){{\/i18n}}/gm) || [];
+        _.each(results, function(result) {
+            var key, value;
+            var r = result.match(/{{#i18n\s*([^}]*)}}((?:(?!{{\/i18n}})(?:.|\n))*){{\/i18n}}/m) || [];
+
+            if ( ! _.isUndefined(r[2])) {
+                value = _.trim(r[2], '\'"');
+            }
+
+            if (_.isUndefined(value)) {
+                return;
+            }
+
+            key = hash(value); // returns a hash value as its default key
+
+            parser.parse(key, value);
+            parseResults.push([key, _.isUndefined(value) ? '' : value ]);
+        });
+    }());
+
+    if (_.size(parseResults) > 1) {
+        gutil.log('result of ' + JSON.stringify(file.relative) + ':\n' + table(parseResults, {'hsep': ' | '}));
+    }
+
+    done();
 };
 
 test('setup', function(t) {
@@ -19,21 +113,25 @@ test('teardown', function(t) {
 });
 
 test('Gulp usage', function(t) {
-    gulp.src(toFixturePath('modules/**/*.{js,hbs}'), {base: 'fixtures'})
-        .pipe(scanner({
-            lngs: ['en','de'],
-            defaultValue: '__STRING_NOT_TRANSLATED__',
-            resGetPath: 'i18n/__lng__/__ns__.json',
-            resSetPath: 'i18n/__lng__/__ns__.savedMissing.json',
-            nsseparator: ':', // namespace separator
-            keyseparator: '.', // key separator
-            interpolationPrefix: '__',
-            interpolationSuffix: '__',
-            ns: {
-                namespaces: [],
-                defaultNs: 'translation'
-            }
-        }))
+    var options = {
+        lngs: ['en','de'],
+        defaultValue: '__STRING_NOT_TRANSLATED__',
+        resGetPath: 'i18n/__lng__/__ns__.json',
+        resSetPath: 'i18n/__lng__/__ns__.savedMissing.json',
+        nsseparator: ':', // namespace separator
+        keyseparator: '.', // key separator
+        interpolationPrefix: '__',
+        interpolationSuffix: '__',
+        ns: {
+            namespaces: [
+                'translation',
+                'locale'
+            ],
+            defaultNs: 'translation'
+        }
+    };
+    gulp.src('test/fixtures/modules/**/*.{js,hbs}')
+        .pipe(i18nextScanner(options, customTransform))
         .pipe(gulp.dest('./output'))
         .on('end', function() {
             t.end();
