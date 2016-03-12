a267d33f59e367ed5d7aa964b01c8b0cdbdaf52a
Cheton Wu <cheton@gmail.com>
2016-03-13 00:18:43 +0800

    Add test for disabled nsseparator

--- test/parser.js
+++ test/parser.js
@@ -9,15 +9,31 @@ var options = {
     lngs: ['en'],
 };
 
-t.plan(2)
+t.plan(4);
+
+t.test('disabled nsseparator', function(t){
+    options.nsseparator = false;
+    options.keyseparator = '.';
+    var parser = i18nextScanner(options, customTransform).parser;
+    parser.parse('foo:bar', '');
+    t.same(parser.resStore, {
+        en: {
+            translation: {
+                'foo:bar': ''
+            }
+        }
+    }, 'The ns should not use default nsseparator : to split');
+    t.end();
+});
 
 t.test('disabled keyseparator', function(t){
+    options.nsseparator = ':';
     options.keyseparator = false;
     var parser = i18nextScanner(options, customTransform).parser;
     parser.parse('Creating...', '');
     t.same(parser.resStore, {
         en: {
-            translation:{
+            translation: {
                 'Creating...': ''
             }
         }
@@ -25,14 +41,33 @@ t.test('disabled keyseparator', function(t){
     t.end();
 });
 
+t.test('default nsseparator', function(t){
+    options.nsseparator = ':';
+    options.keyseparator = '.';
+    var parser = i18nextScanner(options, customTransform).parser;
+    parser.parse('translation:key1.key2', '');
+    console.log(util.inspect(parser.resStore));
+    t.same(parser.resStore, {
+        en: {
+            translation: {
+                'key1': {
+                    'key2': ''
+                }
+            }
+        }
+    }, 'The ns should use default nsseparator : to split');
+    t.end();
+});
+
 t.test('default keyseparator', function(t){
+    options.nsseparator = ':';
     options.keyseparator = '.';
     var parser = i18nextScanner(options, customTransform).parser;
     parser.parse('key1.key2', '');
     console.log(util.inspect(parser.resStore));
     t.same(parser.resStore, {
         en: {
-            translation:{
+            translation: {
                 'key1': {
                     'key2': ''
                 }
