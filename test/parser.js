74b98d4a0d1f9a4ab8a26965b8648817d23bf233
Cheton Wu <cheton@gmail.com>
2016-03-15 00:45:40 +0800

    gettext style i18n

--- test/parser.js
+++ test/parser.js
@@ -1,8 +1,33 @@
+import fs from 'fs';
+import path from 'path';
 import { test } from 'tap';
-import util from 'util';
 import { Parser } from '../src';
 
 const defaults = {};
+
+test('gettext style i18n', (t) => {
+    const parser = new Parser({
+        keySeparator: false,
+        nsSeparator: false
+    });
+
+    // Parse Translation Function
+    const content = fs.readFileSync(path.resolve(__dirname, 'fixtures/modules/index.js'), 'utf8');
+
+    parser.parseFuncFromString(content, { list: ['i18n._'] });
+
+    const resStore = parser.getResourceStore();
+    t.same(resStore, {
+        en: {
+            translation: {
+                "Loading...": "Loading...",
+                "This value does not exist.": "This value does not exist.",
+                "YouTube has more than __count__ billion users.": "YouTube has more than __count__ billion users."
+          }
+        }
+    });
+    t.end();
+});
 
 test('disable nsSeparator', (t) => {
     const parser = new Parser(Object.assign({}, defaults, {
