8337bfd5652892dad5a19ccb1356619028260dd0
Cheton Wu <cheton@gmail.com>
2016-03-15 15:46:42 +0800

    Update tests

--- test/gulp.js
+++ test/gulp.js
@@ -7,13 +7,16 @@ import customTransform from './utils/transform';
 test('Gulp usage', function(t) {
     const options = {
         debug: false,
+        func: {
+            list: ['_t']
+        },
         lngs: ['en','de'],
         ns: ['resource'],
         defaultNs: 'resource',
         defaultValue: '__STRING_NOT_TRANSLATED__',
         resource: {
-            loadPath: path.resolve(__dirname, 'fixtures/i18n/__lng__/__ns__.json'),
-            savePath: 'i18n/__lng__/__ns__.json'
+            loadPath: path.resolve(__dirname, 'fixtures/i18n/{{lng}}/{{ns}}.json'),
+            savePath: 'i18n/{{lng}}/{{ns}}.json'
         },
         nsSeparator: ':', // namespace separator
         keySeparator: '.', // key separator
