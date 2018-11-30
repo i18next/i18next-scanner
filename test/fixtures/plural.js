9c6e4f7c394b0bd338eaccf7df972d2cdb591781
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2018-11-30 17:09:03 +0200

    Handle default value in second argument with placeholders. Fixes #111. (#113)

--- test/fixtures/plural.js
+++ test/fixtures/plural.js
@@ -1,11 +1,15 @@
-i18next.t('key', {count: 0}); // output: 'items'
-i18next.t('key', {count: 1}); // output: 'item'
-i18next.t('key', {count: 5}); // output: 'items'
-i18next.t('key', {count: 100}); // output: 'items'
-i18next.t('keyWithCount', {count: 0}); // output: '0 items'
-i18next.t('keyWithCount', {count: 1}); // output: '1 item'
-i18next.t('keyWithCount', {count: 5}); // output: '5 items'
-i18next.t('keyWithCount', {count: 100}); // output: '100 items'
+i18next.t('key', { count: 0 }); // output: 'items'
+i18next.t('key', { count: 1 }); // output: 'item'
+i18next.t('key', { count: 5 }); // output: 'items'
+i18next.t('key', { count: 100 }); // output: 'items'
+i18next.t('keyWithCount', { count: 0 }); // output: '0 items'
+i18next.t('keyWithCount', { count: 1 }); // output: '1 item'
+i18next.t('keyWithCount', { count: 5 }); // output: '5 items'
+i18next.t('keyWithCount', { count: 100 }); // output: '100 items'
+i18next.t('keyWithDefaultValueAndCount', '{{count}} item', { count: 0 }); // output: '0 items'
+i18next.t('keyWithDefaultValueAndCount', '{{count}} item', { count: 1 }); // output: '0 items'
+i18next.t('keyWithDefaultValueAndCount', '{{count}} item', { count: 5 }); // output: '0 items'
+i18next.t('keyWithDefaultValueAndCount', '{{count}} item', { count: 100 }); // output: '0 items'
 
 // Variables
 const one = 1;
@@ -13,3 +17,5 @@ const count = 2;
 
 i18next.t('keyWithVariable', { count: one }); // output: 'item'
 i18next.t('keyWithVariable', { count }); // output: 'items'
+i18next.t('keyWithDefaultValueAndVariable', '{{count}} item', { count: one }); // output: 'item'
+i18next.t('keyWithDefaultValueAndVariable', '{{count}} item', { count }); // output: 'item'
