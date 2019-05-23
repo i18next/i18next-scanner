eac86df75fd17f2f237b7b416db59e5bb6885d4d
Cheton Wu <cheton@gmail.com>
2019-05-23 19:34:15 +0800

    Workaround with acorn and acorn-dynamic-import (#147) (#149)

--- src/acorn-jsx-walk.js
+++ src/acorn-jsx-walk.js
@@ -1,11 +1,7 @@
 // Originally from: https://github.com/sderosiaux/acorn-jsx-walk
 import { simple as walk, base } from 'acorn-walk';
+import { DynamicImportKey } from 'acorn-dynamic-import';
 
-//
-// Extends acorn walk with JSX elements
-//
-
-// See: https://github.com/RReverser/acorn-jsx/issues/23#issuecomment-403753801
 Object.assign(base, {
     FieldDefinition(node, state, callback) {
         if (node.value !== null) {
@@ -13,6 +9,14 @@ Object.assign(base, {
         }
     },
 
+    // Workaround with `acorn` and `acorn-dynamic-import`
+    // https://github.com/kristoferbaxter/dynamic-walk/blob/master/workaround.js
+    [DynamicImportKey]: () => {},
+});
+
+// Extends acorn walk with JSX elements
+// https://github.com/RReverser/acorn-jsx/issues/23#issuecomment-403753801
+Object.assign(base, {
     JSXAttribute(node, state, callback) {
         if (node.value !== null) {
             callback(node.value, state);
