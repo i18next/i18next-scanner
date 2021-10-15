e481e6ce5c80f845dcf1327a15983ebbf40e7c48
Cheton Wu <cheton@gmail.com>
2021-10-15 22:33:25 +0800

    test: add test fixtures for optional chaining

---
+++ test/fixtures/optional-chaining.js
@@ -0,0 +1,3 @@
+i18n.t('optional chaining: {{value}}', { value: obj.first?.second });
+i18n.t('optional chaining: {{value}}', { value: arr?.[1] });
+i18n.t('optional chaining: {{value}}', { value: someInterface.customerMethod?.() });
