8a29ce56f52fa6e8967b4f19500e4b166c57fcf1
cheton <cheton@gmail.com>
2022-06-19 15:56:32 +0800

    chore: update CLI and add missing dependencies

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
 const path = require('path');
 const program = require('commander');
 const ensureArray = require('ensure-array');
@@ -10,64 +9,67 @@ const scanner = require('../lib').default;
 const pkg = require('../package.json');
 
 program
-    .version(pkg.version)
-    .usage('[options] <file ...>')
-    .option('--config <config>', 'Path to the config file (default: i18next-scanner.config.js)', 'i18next-scanner.config.js')
-    .option('--output <path>', 'Path to the output directory (default: .)');
+  .version(pkg.version)
+  .usage('[options] <file ...>')
+  .option('--config <config>', 'Path to the config file (default: i18next-scanner.config.js)', 'i18next-scanner.config.js')
+  .option('--output <path>', 'Path to the output directory (default: .)');
 
-program.on('--help', function() {
-    console.log('');
-    console.log('  Examples:');
-    console.log('');
-    console.log('    $ i18next-scanner --config i18next-scanner.config.js --output /path/to/output \'src/**/*.{js,jsx}\'');
-    console.log('    $ i18next-scanner --config i18next-scanner.config.js "src/**/*.{js,jsx}"');
-    console.log('    $ i18next-scanner "/path/to/src/app.js" "/path/to/assets/index.html"');
-    console.log('');
+program.on('--help', () => {
+  console.log('');
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ i18next-scanner --config i18next-scanner.config.js --output /path/to/output \'src/**/*.{js,jsx}\'');
+  console.log('    $ i18next-scanner --config i18next-scanner.config.js "src/**/*.{js,jsx}"');
+  console.log('    $ i18next-scanner "/path/to/src/app.js" "/path/to/assets/index.html"');
+  console.log('');
 });
 
 program.parse(process.argv);
 
-if (!program.config) {
-    program.help();
-    return;
+const options = program.opts();
+
+if (!options.config) {
+  program.help();
+  process.exit(1);
 }
 
 let config = {};
 try {
-    config = require(path.resolve(program.config));
+  // eslint-disable-next-line import/no-dynamic-require
+  config = require(path.resolve(options.config));
 } catch (err) {
-    console.error('i18next-scanner:', err);
-    return;
+  console.error('i18next-scanner:', err);
+  process.exit(1);
 }
 
 { // Input
-    config.input = (program.args.length > 0) ? program.args : ensureArray(config.input);
-    config.input = config.input.map(function(s) {
-        s = s.trim();
+  config.input = (program.args.length > 0) ? program.args : ensureArray(config.input);
+  config.input = config.input.map((s) => {
+    s = s.trim();
 
-        // On Windows, arguments contain spaces must be enclosed with double quotes, not single quotes.
-        if (s.match(/(^'.*'$|^".*"$)/)) {
-            // Remove first and last character
-            s = s.slice(1, -1);
-        }
-        return s;
-    });
-
-    if (config.input.length === 0) {
-        program.help();
-        return;
+    // On Windows, arguments contain spaces must be enclosed with double quotes, not single quotes.
+    if (s.match(/(^'.*'$|^".*"$)/)) {
+      // Remove first and last character
+      s = s.slice(1, -1);
     }
+    return s;
+  });
+
+  if (config.input.length === 0) {
+    program.help();
+    process.exit(1);
+  }
 }
 
 { // Output
-    config.output = program.output || config.output;
+  config.output = options.output || config.output;
 
-    if (!config.output) {
-        config.output = '.';
-    }
+  if (!config.output) {
+    config.output = '.';
+  }
 }
 
 vfs.src(config.input)
-    .pipe(sort()) // Sort files in stream by path
-    .pipe(scanner(config.options, config.transform, config.flush))
-    .pipe(vfs.dest(config.output))
+  .pipe(sort()) // Sort files in stream by path
+  .pipe(scanner(config.options, config.transform, config.flush))
+  .pipe(vfs.dest(config.output));
